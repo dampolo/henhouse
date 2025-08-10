79e1bc1e8c0763aa667e871c35f4c0842cc8b61f
Damian Poloczek <damianpoloczek@gmail.com>
2025-08-10 22:02:10 +0200

    renderAnimals()

---
+++ data.js
@@ -0,0 +1,5 @@
+const jsonData = [
+        {name: "Alina", eggs: 2, type: "hen"},
+        {name: "Danuta", eggs: 3, type: "hen"},
+        {name: "Jadwiga", eggs: 5, type: "hen"}
+    ];
--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const selectAnimal = document.getElementById('animals');
-const animalInput = document.querySelector('.name-new-hen')
-const eggsInput = document.querySelector('.eggs-new-hen');
+const selectAnimal = document.getElementById("animals");
+const animalInput = document.querySelector(".name-new-hen");
+const eggsInput = document.querySelector(".eggs-new-hen");
 
-selectAnimal.addEventListener('change', () => {
-    if (selectAnimal.value === 'cock') {
-        eggsInput.disabled = true;
-        eggsInput.value = ''; // optional: clear value
-    } else {
-        eggsInput.disabled = false;
-    }
+selectAnimal.addEventListener("change", () => {
+  if (selectAnimal.value === "cock") {
+    eggsInput.disabled = true;
+    eggsInput.value = ""; // optional: clear value
+  } else {
+    eggsInput.disabled = false;
+  }
 });
 
 function addNewHen() {
-    animalInput.value = "";
-    eggsInput.value = "";
+  animalInput.value = "";
+  eggsInput.value = "";
 }
+
+function renderAnimals() {
+  const allHens = document.querySelector(".amount-of-all-hens");
+  jsonData.forEach((item) => {
+    allHens.innerHTML += /*html*/ `
+    <li>
+        <p>Hen ${item.name} laid ${item.eggs} eggs.</p>
+        <div class="input-button">
+            <input name="number" type="number" min="1">
+            <button>ADD</button>
+        </div>
+    </li> `;
+  });
+}
+
+// Run it
+renderAnimals();
