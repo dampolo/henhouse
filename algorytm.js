5e281832d1583c2c098361652bf467cd46f5cf50
Damian Poloczek <damianpoloczek@gmail.com>
2025-08-15 23:17:16 +0200

    change the name to getNestlingCount()

--- algorytm.js
+++ algorytm.js
@@ -1,7 +1,7 @@
 
 
 
-function nestlingCheck(animal) {
+function getNestlingCount(animal) {
     const basisEgg = 30
     animal.nestling = Math.floor(animal.total_eggs / basisEgg);
     return Math.floor(animal.total_eggs / basisEgg);
