5389c96056bf2096a88998c4d6c72225255fb25a
Damian Poloczek <damianpoloczek@gmail.com>
2025-08-15 23:20:27 +0200

    getNestlingCount() with other return

--- algorytm.js
+++ algorytm.js
@@ -4,5 +4,5 @@
 function getNestlingCount(animal) {
     const basisEgg = 30
     animal.nestling = Math.floor(animal.total_eggs / basisEgg);
-    return Math.floor(animal.total_eggs / basisEgg);
+    return animal.nestling;
 }
