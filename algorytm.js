c667ea80f8d0461ab77d8574959de7057b7b92b8
Damian Poloczek <damianpoloczek@gmail.com>
2025-08-19 07:17:26 +0200

    isTypeRooster() and is isTypeHen()

--- algorytm.js
+++ algorytm.js
@@ -3,7 +3,32 @@
 
 function getNestlingCount(animal) {
     const basisEgg = 30
-    animal.nestling = Math.floor(animal.total_eggs / basisEgg);
-    allNestlings()
-    return animal.nestling;
+    
+    if (isTypeHen() && isTypeRooster()) {
+        animal.nestling = Math.floor(animal.total_eggs / basisEgg);
+        allNestlings();
+        return animal.nestling;
+    }
+}
+
+
+/**
+ * Checks if there is at least one rooster in the henHouse.
+ *
+ * @param {*} params - Currently unused, placeholder for potential future parameters.
+ * @returns {boolean} True if at least one animal in henHouse has type "rooster", otherwise false.
+ */
+function isTypeRooster(params) {
+    return henHouse.allAnimals.some((isrooster) => isrooster.type === "rooster")   
+}
+
+
+/**
+ * Checks if there is at least one hen in the henHouse.
+ *
+ * @param {*} params - Currently unused, placeholder for potential future parameters.
+ * @returns {boolean} True if at least one animal in henHouse has type "rooster", otherwise false.
+ */
+function isTypeHen(params) {
+    return henHouse.allAnimals.some((ishen) => ishen.type === "hen");
 }
