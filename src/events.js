7ae4085f52ba0f256c80b5809374d53be9ceb716
Damian Poloczek <damianpoloczek@gmail.com>
2025-09-03 12:40:25 +0200

    attachedEvents() extends with collectAllEggs()

--- src/events.js
+++ src/events.js
@@ -1,6 +1,6 @@
-import { breakHenHouse } from "./script";
+import { breakHenHouse, collectAllEggs } from "./script";
 
 export default function attachedEvents() {
     document.querySelector(".break-btn").addEventListener("click", () => breakHenHouse());
+    document.querySelector(".collect-all-eggs").addEventListener("click", () => collectAllEggs())
 }
-
