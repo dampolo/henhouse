2af53b639fd92f056b564aaede9c74101570aec3
Damian Poloczek <damianpoloczek@gmail.com>
2025-09-03 13:05:31 +0200

    attachedEvents() extends with activeReactiveButton()

--- src/events.js
+++ src/events.js
@@ -1,7 +1,8 @@
-import { breakHenHouse, collectAllEggs, collectEggs } from "./script";
+import { activeReactiveButton, breakHenHouse, collectAllEggs, collectEggs } from "./script";
 
 export default function attachedEvents() {
     document.querySelector(".break-btn").addEventListener("click", () => breakHenHouse());
     document.querySelector(".collect-some-eggs-main-button").addEventListener("click", () => collectEggs());
     document.querySelector(".collect-all-eggs").addEventListener("click", () => collectAllEggs());
+    document.querySelector(".number-of-eggs").addEventListener("input", () => activeReactiveButton())
 }
