2755093355f3652af9858811eb94d167ae7e0f11
Damian Poloczek <damianpoloczek@gmail.com>
2025-09-03 13:20:25 +0200

    attachedEvents() extends with addNewAnimal() and changed the from

--- src/events.js
+++ src/events.js
@@ -1,8 +1,12 @@
-import { activeReactiveButton, breakHenHouse, collectAllEggs, collectEggs } from "./script";
+import { activeReactiveButton, addNewAnimal, breakHenHouse, collectAllEggs, collectEggs } from "./script";
 
 export default function attachedEvents() {
     document.querySelector(".break-btn").addEventListener("click", () => breakHenHouse());
     document.querySelector(".collect-some-eggs-main-button").addEventListener("click", () => collectEggs());
     document.querySelector(".collect-all-eggs").addEventListener("click", () => collectAllEggs());
-    document.querySelector(".number-of-eggs").addEventListener("input", () => activeReactiveButton())
+    document.querySelector(".number-of-eggs").addEventListener("input", () => activeReactiveButton());
+    document.querySelector(".add-new-animal").addEventListener("submit", (event) => {
+        event.preventDefault();
+        addNewAnimal();
+    })
 }
